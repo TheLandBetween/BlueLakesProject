73f730e83ae3b0c5f30882b21230ed691ed16bed
Jared <jmezzbmxer@live.ca>
2020-11-28 19:57:51 -0500

    removing required fields from lake health report

--- views/models/lakehealthreport.js
+++ views/models/lakehealthreport.js
@@ -3,43 +3,34 @@ const mongoose = require('mongoose');
 // create a template for the table (layed out in the db schema)
 const lakeHealthReportSchema = new mongoose.Schema({
     Date_Generated: {
-        type: String,
-        required: String
+        type: String
     },
     Status: {
-        type: String,
-        required: true
+        type: String
     },
     Summary: {
-        type: String,
-        required: true
+        type: String
     },
     Level_of_Concern: {
         type: Number,
-        required: true,
-        min: 0,
+        min: 0
         // if we wanted to do only between 0 and 10, would setup this way
         // enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     },
     Perc_Shore_Devd: {
-        type: Number,
-        required: true
+        type: Number
     },
     Avg_Temp: {
-        type: Number,
-        required: true
+        type: Number
     },
     Avg_DO_Conc: {
-        type: Number,
-        required: true
+        type: Number
     },
     Avg_Secchi_Depth: {
-        type: Number,
-        required: true
+        type: Number
     },
     Avg_Phosph: {
-        type: Number,
-        required: true
+        type: Number
     }
 });
 
