04e504edebbb4622db3d91594f0d2069d43d32d6
MichaelDesktop <mtdnichol@gmail.com>
2020-11-30 03:40:25 -0500

    Ported anglerReports to routing.  Removed angler reports option from the taskbar until I fix the functionality.

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ const mongoose = require('mongoose');
 // setup lake health report model + route
 const LakeHealthReport = require(path.join(__dirname, "views/models/Lake_Health_Report")); // TODO: may not need this here
 const lakeReportRoutes = require('./routes/lakeReports');
+const anglerReportRoutes = require('./routes/anglerReports');
 // setup angler report model + route
-const AnglerReport = require(path.join(__dirname, "views/models/Angler_Report"));
-// const anglerReportRoutes = require('./routes/anglerReports');
 
 // connect to "BlueLakes" database
 mongoose.connect('mongodb://localhost:27017/BlueLakes', {useNewUrlParser: true, useUnifiedTopology: true})
@@ -81,77 +80,13 @@ app.get('/', (req, res) => {
 
 //LAKE HEALTH REPORT ROUTING
 app.use('/lakeReports', lakeReportRoutes);
-
 app.get('/lakeReports/:id/edit', (req, res) => {
-
 });
 
 //ANGLER REPORT ROUTING
-// GET /anglerReports - list all anglerReports
-app.get('/anglerReports', async (req, res) => {
-    // async callback to wait for health lakeReports to be received, then respond with webpage
-    const anglerReports = await AnglerReport.find({});
-    // render index.ejs file with the lakeReports 'database'
-    res.render('anglerReports/index', { anglerReports, levelDeep: levelDeep = true});
-});
-
-// POST /anglerReports - Create new report
-app.get('/anglerReports/new', (req, res) => {
-    // render new report page
-    res.render('anglerReports/new', {levelDeep: levelDeep = true});
-});
-// on anglerReports/new submission it posts to /lakeReports
-app.post('/anglerReports', async (req, res) => {
-    // TODO: Error handle this acception of the req.body. not checking if extra is passed in (sanatize etc)
-    // assigns passed in form to a lake health report object, saving to a variable
-    const newReport = new AnglerReport(req.body);
-    await newReport.save();
-    // redirect back to view all lakeReports page
-    // redirect to avoid form resubmission on refresh
-    res.redirect(`/anglerReports/${newReport._id}`);
-});
-
-// show route
-// GET /anglerReports/:id - Get one report (using ID)
-// TODO: Slugify link at some point, so instead of id in the url it can be something realative to the report (name / date)
-app.get('/anglerReports/:id', async (req, res) => {
-    // pull id from url
-    const { id } = req.params;
-    // look up the health report corresponding to the id passed in to the url
-    const foundReport = await AnglerReport.findById(id);
-    // send them to the page about the single report
-    res.render('anglerReport/details', { foundReport, levelDeep: levelDeep = true });
-});
-
-// update route -- not sure if really required for our app. do we need to update a report once submitted?
-// PATCH /anglerReports/:id - Update one report
-// using patch as its used to partially modify something, rather than put a whole new report
-app.patch('/anglerReports/:id', (req, res) => {
-    // take id based on url
-    const { id } = req.params;
-    // save updated date sent in request
-    const newReportDate = req.body.date;
-    // match report in 'database' based on id in url
-    const foundReport = reports.find(report => report.id === id);
-    // update report date
-    foundReport.date = newReportDate;
-    // send back to all comments
-    res.redirect('/anglerReports');
-});
-
-// EDIT ROUTE
-// anglerReports/:id/edit
+app.use('/anglerReports', anglerReportRoutes);
 app.get('/anglerReports/:id/edit', (req, res) => {
-    const { id } = req.params;
-    const foundReport = reports.find(report => report.id === id);
-    res.render('anglerReports/edit', { foundReport, levelDeep: levelDeep = true })
 });
-
-// delete route
-// DELETE /anglerReports/:id - Delete one report
-
-
-
 
 // catch all for /path that is not defined -- 404 page
 app.get("*", (req, res) => {
