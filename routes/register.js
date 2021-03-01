67a62c087d73d8cb9fcec9ca6ac1b40b04800d8b
MichaelDesktop <mtdnichol@gmail.com>
2021-02-28 21:56:47 -0500

    rando formatting

--- routes/register.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path'); // duplicated in index.js, need to replace with partial that includes
-
-router.post('/')
