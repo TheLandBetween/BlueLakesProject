4202e7e23552ef0a1374176f8ee884ea810fe481
Jared <jmezzbmxer@live.ca>
2021-03-19 01:38:48 -0400

    add fish button adds new fish card. submit still doesnt work

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,12 @@
-let myVar = 6
+let myVar = 3;
 
-function addFish() {
-
+function addFish(fishCount, emptyFishNode) {
+    var container = $("#fishes")[0];
+    container.load("../../views/partials/fish.ejs");
+//     console.log(container);
+//     container.appendChild(emptyFishNode);
+//     console.log("here");
+//     console.log(emptyFishNode);
+//     console.log(container.childNodes);
+//     container.append('p');
 }
